4f027ed6fcdd8c7e4a2eb7dbb6f6fc59516b3a6c
Amy Cash <amymcash@gmail.com>
2023-01-19 20:18:55 -0800

    Add package component images for rt module

--- olasimple/operation_types/olasimple_rt/protocol.rb
+++ olasimple/operation_types/olasimple_rt/protocol.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# RT module updated March 16, 2022
+# RT module updated January 19, 2023
 needs 'OLASimple/OLAConstants'
 needs 'OLASimple/OLALib'
 needs 'OLASimple/OLAGraphics'
@@ -171,10 +171,24 @@ class Protocol
             end # ops each do
         # inthis case there are no subpackages
         show_open_package(kit_and_unit, '', 0) do
+            rt_tube_labels = ops.map { |op| op.output_tube_label(OUTPUT) }
+            num_samples = 2
+            grid = SVGGrid.new(num_samples, 1, 75, 10)
+            rt_tube_labels.each_with_index do |tube_label, i|
+                rt_tube = make_tube(closedtube, '', tube_label, 'powder', true).scale(0.75)
+                grid.add(rt_tube, i, 0)
+            end # rt_tube labels each do
+            img = SVGElement.new(children: [grid], boundx: 1000, boundy: 300)
+            check 'Check that the following are in the pack:'
+            note display_svg(img, 0.75)
+            
+            
+
+            check 'Discard the packaging material.'
         end #show_open_package do
       end #grouped by unit
     end #method
-    
+  
     def centrifuge_samples(ops)
         #labels = ops.map { |op| ref(op.output(OUTPUT).item) }
         show do
@@ -196,11 +210,13 @@ class Protocol
             to = ref(op.output(OUTPUT).item)
             # tubeS = make_tube(opentube, [SAMPLE_ALIAS, from], '', fluid = 'medium')
             tubeS = make_tube(opentube, [from], '', fluid = 'medium')
+            tubeP = make_tube(opentube, [to], '', fluid = 'medium').scale!(0.75)
             tubeP = make_tube(opentube, [to], '', fluid = 'powder').scale!(0.75)
-
+            
+            
             #tubeS_closed = make_tube(closedtube, [SAMPLE_ALIAS, from], '', fluid = 'medium')
-            #tubeS_closed = make_tube(closedtube, [from], '', fluid = 'medium')
-            #tubeP_closed = make_tube(closedtube, [to], '', fluid = 'medium').translate!(0,40).scale!(0.75)
+            # tubeS_closed = make_tube(closedtube, [from], '', fluid = 'medium')
+            # tubeP_closed = make_tube(closedtube, [to], '', fluid = 'medium').translate!(0,40).scale!(0.75)
 
             #pre_transfer_validation_with_multiple_tries(from, to, tubeS_closed, tubeP_closed)
 
@@ -295,3 +311,6 @@ class Protocol
   end #conclusion
 
 end #class
+
+
+
